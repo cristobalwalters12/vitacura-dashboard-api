0a12859efd321a3727542e5aeebcfd3e90b09223
cristobal walters <cristobal.walters@cencosudscotiabank.cl>
2026-08-06 23:43:09 -0400

    test_commit

--- src/database/schemas/alerta.schema.ts
+++ src/database/schemas/alerta.schema.ts
@@ -1,9 +1,9 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
 
 export type AlertaDocument = HydratedDocument<Alerta>;
-
-@Schema({ collection: 'alertas', strict: false })
+//test
+@Schema({ collection: "alertas", strict: false })
 export class Alerta {}
 
 export const AlertaSchema = SchemaFactory.createForClass(Alerta);
